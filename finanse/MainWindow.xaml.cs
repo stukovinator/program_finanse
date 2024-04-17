598cfbac42260dce43d82d3da344764245107f63
Krzysztof Denicki <denickikrzysztof@gmail.com>
2024-04-17 23:21:44 +0200

    Aktualizacja 17.04

--- finanse/MainWindow.xaml.cs
+++ finanse/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Reflection;
 using System.Windows.Forms.DataVisualization.Charting;
-using LiveCharts.Wpf.Charts.Base;
+using System.Globalization;
 //using DataVis = System.Windows.Forms.DataVisualization;
 
 namespace finanse
@@ -42,7 +42,7 @@ namespace finanse
             {
                 string workingDirectory = Environment.CurrentDirectory;
                 string folderPath = System.IO.Directory.GetParent(workingDirectory).Parent.Parent.FullName;
-                string filePath = System.IO.Path.Combine(folderPath, "finanse", "transakcje.txt");
+                string filePath = System.IO.Path.Combine(folderPath, "finanse", "wydatki.txt");
 
                 if (!File.Exists(filePath))
                 {
@@ -57,14 +57,21 @@ namespace finanse
 
                 for (int i = 0; i < 5; i++)
                 {
-                    if (line == null) break;
+                    if (line == null)
+                    {
+                        Console.WriteLine("pusta linijka");
+                        break;
+                    }
 
                     string[] elements = line.Split(';');
 
-                    if (elements.Length == 2)
+                    if (elements.Length == 3)
                     {
                         string name = elements[0];
                         string value = elements[1];
+                        //string category = elements[2];
+
+                        //Console.WriteLine($"{name} {value} {category}");
 
                         System.Windows.Media.Color bc = System.Windows.Media.Color.FromRgb(36, 36, 36);
                         System.Windows.Media.Color c1 = System.Windows.Media.Color.FromRgb(103, 27, 145);
@@ -123,7 +130,266 @@ namespace finanse
             }
             finally
             {
-                //Console.WriteLine("Wypisano 5 transakcji");
+                //Console.WriteLine("Wypisano 5 wydatkow");
+            }
+        }
+    }
+
+    public class TransactionsPanelExpenses
+    {
+        public decimal total_expenses;
+        public decimal shopping;
+        public decimal services;
+        public decimal payments;
+
+        public TransactionsPanelExpenses()
+        {
+            total_expenses = 0;
+        }
+
+        public void writeAllExpenses(StackPanel list)
+        {
+            string line;
+
+            try
+            {
+                string workingDirectory = Environment.CurrentDirectory;
+                string folderPath = System.IO.Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+                string filePath = System.IO.Path.Combine(folderPath, "finanse", "wydatki.txt");
+
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("Plik nie istnieje");
+                    return;
+                }
+
+                StreamReader sr = new StreamReader(filePath);
+                line = sr.ReadLine();
+
+                while(line != null)
+                {
+                    string[] elements = line.Split(';');
+
+                    if (elements.Length == 3)
+                    {
+                        string name = elements[0];
+                        string value = elements[1];
+                        string type = elements[2];
+
+                        System.Windows.Media.Color bc = System.Windows.Media.Color.FromRgb(36, 36, 36);
+                        System.Windows.Media.Color c1 = System.Windows.Media.Color.FromRgb(103, 27, 145);
+                        System.Windows.Media.Color c2 = System.Windows.Media.Color.FromRgb(98, 11, 179);
+
+                        Border border = new Border();
+                        border.Background = new SolidColorBrush(bc);
+                        border.BorderThickness = new Thickness(2);
+                        border.CornerRadius = new CornerRadius(3);
+                        border.Margin = new Thickness(5, 1, 5, 5);
+                        border.Height = 39;
+                        border.Width = 235;
+                        border.BorderBrush = new LinearGradientBrush(c1, c2, new System.Windows.Point(0.5, 0), new System.Windows.Point(0.5, 1));
+
+                        StackPanel sp = new StackPanel();
+                        sp.Margin = new Thickness(0);
+
+                        System.Windows.Controls.Grid grid = new System.Windows.Controls.Grid();
+                        grid.Margin = new Thickness(0, 5, 0, 5);
+                        grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                        grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+
+                        Label nameLabel = new Label();
+                        nameLabel.Content = name.ToUpper();
+                        nameLabel.HorizontalContentAlignment = HorizontalAlignment.Left;
+                        nameLabel.Margin = new Thickness(10, 0, 0, 0);
+                        nameLabel.Foreground = System.Windows.Media.Brushes.White;
+                        nameLabel.FontFamily = new System.Windows.Media.FontFamily(folderPath + "\\finanse\\fonts\\#Roboto Medium");
+                        System.Windows.Controls.Grid.SetColumn(nameLabel, 0);
+
+                        Label valueLabel = new Label();
+                        valueLabel.Content = "-" + value;
+                        valueLabel.HorizontalContentAlignment = HorizontalAlignment.Right;
+                        valueLabel.Margin = new Thickness(0, 0, 10, 0);
+                        valueLabel.Foreground = System.Windows.Media.Brushes.IndianRed;
+                        valueLabel.FontFamily = new System.Windows.Media.FontFamily(folderPath + "\\finanse\\fonts\\#Roboto Regular");
+                        System.Windows.Controls.Grid.SetColumn(valueLabel, 1);
+
+                        grid.Children.Add(nameLabel);
+                        grid.Children.Add(valueLabel);
+
+                        sp.Children.Add(grid);
+
+                        border.Child = sp;
+
+                        list.Children.Add(border);
+
+                        decimal value_numeric;
+
+                        if(decimal.TryParse(value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out value_numeric))
+                        {
+                            switch (type)
+                            {
+                                case "zakupy":
+                                    this.shopping += value_numeric;
+                                    break;
+                                case "uslugi":
+                                    this.services += value_numeric;
+                                    break;
+                                case "oplata":
+                                    this.payments += value_numeric;
+                                    break;
+                            }
+                            this.total_expenses += value_numeric;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nieprawidłowy format liczby");
+                        }
+                        
+                    }
+
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine($"zakupy: {this.shopping}  uslugi: {this.services}  oplaty: {this.payments}");
+            }
+        }
+    }
+
+    public class TransactionsPanelEarnings
+    {
+        public decimal total_earnings;
+        public decimal salary;
+        public decimal refunds;
+        public decimal transfers;
+        public decimal deposits;
+
+        public TransactionsPanelEarnings()
+        {
+            total_earnings = 0;
+        }
+
+        public void writeAllEarnings(StackPanel list)
+        {
+            string line;
+
+            try
+            {
+                string workingDirectory = Environment.CurrentDirectory;
+                string folderPath = System.IO.Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+                string filePath = System.IO.Path.Combine(folderPath, "finanse", "zarobki.txt");
+
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("Plik nie istnieje");
+                    return;
+                }
+
+                StreamReader sr = new StreamReader(filePath);
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    string[] elements = line.Split(';');
+
+                    if (elements.Length == 3)
+                    {
+                        string name = elements[0];
+                        string value = elements[1];
+                        string type = elements[2];
+
+                        System.Windows.Media.Color bc = System.Windows.Media.Color.FromRgb(36, 36, 36);
+                        System.Windows.Media.Color c1 = System.Windows.Media.Color.FromRgb(103, 27, 145);
+                        System.Windows.Media.Color c2 = System.Windows.Media.Color.FromRgb(98, 11, 179);
+
+                        Border border = new Border();
+                        border.Background = new SolidColorBrush(bc);
+                        border.BorderThickness = new Thickness(2);
+                        border.CornerRadius = new CornerRadius(3);
+                        border.Margin = new Thickness(5, 1, 5, 5);
+                        border.Height = 39;
+                        border.Width = 235;
+                        border.BorderBrush = new LinearGradientBrush(c1, c2, new System.Windows.Point(0.5, 0), new System.Windows.Point(0.5, 1));
+
+                        StackPanel sp = new StackPanel();
+                        sp.Margin = new Thickness(0);
+
+                        System.Windows.Controls.Grid grid = new System.Windows.Controls.Grid();
+                        grid.Margin = new Thickness(0, 5, 0, 5);
+                        grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                        grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+
+                        Label nameLabel = new Label();
+                        nameLabel.Content = name.ToUpper();
+                        nameLabel.HorizontalContentAlignment = HorizontalAlignment.Left;
+                        nameLabel.Margin = new Thickness(10, 0, 0, 0);
+                        nameLabel.Foreground = System.Windows.Media.Brushes.White;
+                        nameLabel.FontFamily = new System.Windows.Media.FontFamily(folderPath + "\\finanse\\fonts\\#Roboto Medium");
+                        System.Windows.Controls.Grid.SetColumn(nameLabel, 0);
+
+                        Label valueLabel = new Label();
+                        valueLabel.Content = "+" + value;
+                        valueLabel.HorizontalContentAlignment = HorizontalAlignment.Right;
+                        valueLabel.Margin = new Thickness(0, 0, 10, 0);
+                        valueLabel.Foreground = System.Windows.Media.Brushes.LightGreen;
+                        valueLabel.FontFamily = new System.Windows.Media.FontFamily(folderPath + "\\finanse\\fonts\\#Roboto Regular");
+                        System.Windows.Controls.Grid.SetColumn(valueLabel, 1);
+
+                        grid.Children.Add(nameLabel);
+                        grid.Children.Add(valueLabel);
+
+                        sp.Children.Add(grid);
+
+                        border.Child = sp;
+
+                        list.Children.Add(border);
+
+                        decimal value_numeric;
+
+                        if (decimal.TryParse(value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out value_numeric))
+                        {
+                            switch (type)
+                            {
+                                case "wyplata":
+                                    this.salary += value_numeric;
+                                    break;
+                                case "zwrot":
+                                    this.refunds += value_numeric;
+                                    break;
+                                case "przelew":
+                                    this.transfers += value_numeric;
+                                    break;
+                                case "wplata":
+                                    this.deposits += value_numeric;
+                                    break;
+                            }
+                            this.total_earnings += value_numeric;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nieprawidłowy format liczby");
+                        }
+                    }
+
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine($"wyplata: {this.salary}  zwroty: {this.refunds}  przelewy: {this.transfers}  wplaty: {this.deposits}");
             }
         }
     }
@@ -161,6 +427,85 @@ namespace finanse
         }
     }
 
+    public class TabNavigation
+    {
+        public StackPanel hometab;
+        public StackPanel transactionstab;
+        public StackPanel paymentstab;
+        public StackPanel statstab;
+
+        public TabNavigation()
+        {
+            this.hometab = null;
+            this.transactionstab = null;
+            this.paymentstab = null;
+            this.statstab = null;
+        }
+
+        public TabNavigation(StackPanel hometab, StackPanel transactionstab, StackPanel paymentstab, StackPanel statstab)
+        {
+            this.hometab = hometab;
+            this.transactionstab = transactionstab;
+            this.paymentstab = paymentstab;
+            this.statstab = statstab;
+        }
+
+        public void homeButton()
+        {
+            this.hometab.Visibility = Visibility.Visible;
+            this.transactionstab.Visibility = Visibility.Collapsed;
+            this.paymentstab.Visibility = Visibility.Collapsed;
+            this.statstab.Visibility = Visibility.Collapsed;
+        }
+
+        public void transactionsButton()
+        {
+            this.hometab.Visibility = Visibility.Collapsed;
+            this.transactionstab.Visibility = Visibility.Visible;
+            this.paymentstab.Visibility = Visibility.Collapsed;
+            this.statstab.Visibility = Visibility.Collapsed;
+        }
+
+        public void paymentsButton()
+        {
+            this.hometab.Visibility = Visibility.Collapsed;
+            this.transactionstab.Visibility = Visibility.Collapsed;
+            this.paymentstab.Visibility = Visibility.Visible;
+            this.statstab.Visibility = Visibility.Collapsed;
+        }
+
+        public void statsButton()
+        {
+            this.hometab.Visibility = Visibility.Collapsed;
+            this.transactionstab.Visibility = Visibility.Collapsed;
+            this.paymentstab.Visibility = Visibility.Collapsed;
+            this.statstab.Visibility = Visibility.Visible;
+        }
+    }
+
+    public class Statistics
+    {
+        public Statistics()
+        {
+
+        }
+
+        public void writeExpenses()
+        {
+
+        }
+
+        public void writeEarnings()
+        {
+
+        }
+
+        public void drawChart(System.Windows.Forms.DataVisualization.Charting.Chart chart)
+        {
+
+        }
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -172,6 +517,36 @@ namespace finanse
 
             StarterPanelChart spc = new StarterPanelChart();
             spc.fillChart(chart1);
+
+            TransactionsPanelExpenses tpex = new TransactionsPanelExpenses();
+            tpex.writeAllExpenses(ALL_EXPENSES_LIST);
+
+            TransactionsPanelEarnings tper = new TransactionsPanelEarnings();
+            tper.writeAllEarnings(ALL_EARNINGS_LIST);
         }
+
+        private void navigationButton(object sender, MouseButtonEventArgs e)
+        {
+            Border btn = sender as Border;
+
+            TabNavigation tabNavigation = new TabNavigation(HOME_TAB, TRANSACTIONS_TAB, PAYMENTS_TAB, STATS_TAB);
+
+            switch (btn.Tag)
+            {
+                case "HOME_BTN":
+                    tabNavigation.homeButton();
+                    break;
+                case "TRANS_BTN":
+                    tabNavigation.transactionsButton();
+                    break;
+                case "PAYMENTS_BTN":
+                    tabNavigation.paymentsButton();
+                    break;
+                case "STATS_BTN":
+                    tabNavigation.statsButton();
+                    break;
+            }
+        }
+
     }
 }
